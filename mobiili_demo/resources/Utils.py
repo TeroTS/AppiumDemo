ed7fb6197b01966faa2b325979c2c1b874167c46
eficode <eficode@eficodes-MacBook-Pro-2.local>
2017-04-06 01:17:54 +0300

    Fixed adb command

--- mobiili_demo/resources/Utils.py
+++ mobiili_demo/resources/Utils.py
@@ -3,5 +3,5 @@ import subprocess
 class Utils(object):
 
     def send_keys_adb(self, id, text):
-        command = "/Users/teststation/teststation/android-sdk/platform-tools/adb -s {} shell input keyboard text {}".format(id, text)
+        command = "/Users/teststation/teststation/android-sdk/platform-tools/adb -s {} shell input touchscreen text {}".format(id, text)
         subprocess.call(command, shell=True)
